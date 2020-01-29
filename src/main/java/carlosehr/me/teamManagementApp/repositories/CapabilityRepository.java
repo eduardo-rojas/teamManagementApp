c5ada870d41fda1e92940a87937ff690269c2ba8
Carlos Hoyos <carlos.edhr@comunidad.unam.mx>
2020-01-28 16:33:28 -0800

    Capability entity and repository created

---
+++ src/main/java/carlosehr/me/teamManagementApp/repositories/CapabilityRepository.java
@@ -0,0 +1,9 @@
+package carlosehr.me.teamManagementApp.repositories;
+
+import carlosehr.me.teamManagementApp.domain.Capability;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CapabilityRepository extends JpaRepository<Capability, Long> {
+
+
+}
---
+++ src/test/java/carlosehr/me/teamManagementApp/TeamManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package carlosehr.me.teamManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
